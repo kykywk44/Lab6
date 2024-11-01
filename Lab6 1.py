2d8574bc6078cbe5f68d3686f2ea372aba1636d2
Denis <dbykov965@gmail.com>
2024-11-01 10:04:06 +0300

    Update Lab6 1.py

--- Lab6 1.py
+++ Lab6 1.py
@@ -7,16 +7,13 @@ class UserAccount:
 
     def set_password(self, new_password):
         self.password = new_password
-        p = ''
-        for a in range (len(self.password)):
-            p = p + '*'
-        print('Пароль был изменен: ',p)
+        print('Пароль был изменен')
 
     def check_password(self, password):
         print(self.password == password)
 
-user1 = UserAccount('Kazimir','kazimirovich@gmail.com','zalupko')
-user1.set_password('zalupenko!')
+user1 = UserAccount('Denis','dbykov1307@gmail.com','denis2010')
+user1.set_password('denis2025')
 
 i = input('Введите пароль для проверки: ')
 user1.check_password(i)
